a65bd6c5c564969fbb7c1a37f114dc9441bb6925
Dmitry841 <strelx@mail.ru>
2020-09-21 11:24:52 +0300

    change brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,21 +4,13 @@ import GenerateRandomNumber from '../utils.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrimeNumber = (num) => {
-  const primeNumber2 = 2;
-  const primeNumber3 = 3;
-  const correct = 'yes';
-  const incorrect = 'no';
-  if (num === 1) {
-    return incorrect;
-  }
-  if (num === primeNumber2 || num === primeNumber3) {
-    return correct;
-  }
-  for (let divider = 2; divider <= num / 2; divider += 1) {
+  if (num < 2) {
+    return false;
+  } for (let divider = 2; divider <= num / 2; divider += 1) {
     if (num % divider === 0) {
-      return incorrect;
+      return false;
     }
-  } return correct;
+  } return true;
 };
 
 const generateData = () => {
@@ -26,7 +18,7 @@ const generateData = () => {
   const lowerRange = 5;
   const upperRange = 50;
   const randomNumber = GenerateRandomNumber(lowerRange, upperRange);
-  const result = isPrimeNumber(randomNumber);
+  const result = isPrimeNumber(randomNumber) ? 'yes' : 'no';
   resultAndAnswer.push(result, randomNumber);
   return resultAndAnswer;
 };
