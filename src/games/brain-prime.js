8fdf4ec21bc1a3a15ab72f1b7b5138dc3d118a1d
Dmitry841 <57747613+Dmitry841@users.noreply.github.com>
2020-09-25 12:39:38 +0300

    Update brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,7 +4,8 @@ import getRandomInt from '../utils.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrimeNumber = (num) => {
-  if (num < 2) {
+  const leastPrimeNum = 2;
+  if (num < leastPrimeNum) {
     return false;
   } for (let divider = 2; divider <= num / 2; divider += 1) {
     if (num % divider === 0) {
