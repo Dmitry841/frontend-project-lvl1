c4bea191a4fb8ed65bb0ca52fd2a7852e0fda594
Dmitry841 <57747613+Dmitry841@users.noreply.github.com>
2020-09-25 12:41:54 +0300

    Update brain-progression.js

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -9,11 +9,12 @@ const lastIndexOfSeries = 9;
 const lowerRange = 5;
 const upperRange = 50;
 
+const stepOfProgression = 5;
+
 const GenerateNumericalSeries = () => {
 
   const firstNumber = getRandomInt(lowerRange, upperRange);
   const Series = [firstNumber];
-  const stepOfProgression = 5;
   for (let ElementOfSeries = firstIndexOfSeries;
     ElementOfSeries < lastIndexOfSeries; ElementOfSeries += 1) {
     Series.push(Series[ElementOfSeries] + stepOfProgression);
