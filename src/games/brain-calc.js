c13e35f10a1dc4cfbc703d674c2f66a671a99d81
Dmitry841 <57747613+Dmitry841@users.noreply.github.com>
2020-09-25 13:33:12 +0300

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -5,9 +5,9 @@ const task = 'What is the result of the expression?';
 
  const minNumber = 1;
  const maxNumber = 10;
+ const sign = ['+', '-', '*'];
 
 const generateData = () => {
-  const sign = ['+', '-', '*'];
   const beginOfSign = sign[0]; // я думаю, что эта константа отностся к лексическому контексту.
   const endOfSign = sign[sign.length - 1];   // я думаю, что эта константа отностся к лексическому контексту.
   const RandomSign = sign[getRandomInt(beginOfSign, endOfSign)];
