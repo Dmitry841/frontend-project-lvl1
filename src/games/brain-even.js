69e9aeaede0f7e02ece3d98716d7786b05065d00
Dmitry841 <57747613+Dmitry841@users.noreply.github.com>
2020-09-25 12:28:56 +0300

    Update brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,10 +5,11 @@ const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (num) => num % 2 === 0;
 
-const generateData = () => {
-  const resultAndAnswer = [];
   const lowerRange = 1;
   const upperRange = 30;
+
+const generateData = () => {
+  const resultAndAnswer = [];
   const randomNumber = getRandomInt(lowerRange, upperRange);
   const result = isEven(randomNumber) ? 'yes' : 'no';
   resultAndAnswer.push(result, randomNumber);
