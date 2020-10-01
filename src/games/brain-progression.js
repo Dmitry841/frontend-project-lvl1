ba8394fa53c48580bcb702d0c54be741b4c8a177
Dmitry841 <strelx@mail.ru>
2020-10-01 09:01:46 +0300

    refactoring

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -9,7 +9,7 @@ const lowerRange = 5;
 const upperRange = 50;
 
 const makeProgression = (firstMember, step) => {
-  const progression = [firstNum];
+  const progression = [firstMember];
   for (let index = 0; // если остaвить только длину прогрессии(L6), это число не стнет "мaгическим"?
     index < lengthOfProgression; index += 1) {
     progression.push(progression[index] + step);
@@ -18,8 +18,10 @@ const makeProgression = (firstMember, step) => {
 };
 
 const generateProgression = () => {
-  const firstMember = getRandomInt(lowerRange, upperRange); // Есть смысл создавать константу или сразу функцию getRandomInt вставить в аргумент?
-  const stepProgression = getRandomInt(lowerRange, upperRange); // То же самое. Если вставить в аргумент, как-будто читабельность ухудшится.
+  const firstMember = getRandomInt(lowerRange, upperRange);
+  const stepProgression = getRandomInt(lowerRange, upperRange);
+  // Есть смысл создавать константу или сразу функцию getRandomInt вставить в аргумент?
+  // Если вставить в аргумент, как-будто читабельность ухудшится.
   const progression = makeProgression(firstMember, stepProgression);
   const hiddenIndex = getRandomInt(0, lengthOfProgression); // если остaвить только длину(L6)
   const copyOfProgression = progression.slice(); // прогрессии, это число не стнет "мaгическим"?
